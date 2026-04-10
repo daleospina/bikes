90f0f76e30d650f6250bcb6f0572c0b6ec773205
diego.ospina <diego.ospina@softwareone.com>
2026-04-10 17:03:28 -0500

    Context factory fixed.

--- src/cicloso.bikes.core/Data/BikesContextFactory.cs
+++ src/cicloso.bikes.core/Data/BikesContextFactory.cs
@@ -9,24 +9,10 @@ public class BikesContextFactory : IDesignTimeDbContextFactory<BikesContext>
 {
     public BikesContext CreateDbContext(string[] args)
     {
-        string? connectionString = null;
-
-        for (int i = 0; i < args.Length; i++)
-        {
-            if (args[i] == "--connection" && i + 1 < args.Length)
-            {
-                connectionString = args[i + 1];
-                break;
-            }
-        }
-
-        connectionString ??= Environment.GetEnvironmentVariable("BikesContext");
-
-        if (string.IsNullOrEmpty(connectionString))
-            throw new Exception("No connection string provided.");
-
+        // Connection string dummy SOLO para poder crear el contexto
         var optionsBuilder = new DbContextOptionsBuilder<BikesContext>();
-        optionsBuilder.UseSqlServer(connectionString);
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=Dummy;User Id=sa;Password=Dummy123!;TrustServerCertificate=True");
 
         return new BikesContext(optionsBuilder.Options);
     }
