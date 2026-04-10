bd42a68a7f86f6e05b0d599c30b0ec5e5b536626
diego.ospina <diego.ospina@softwareone.com>
2026-04-10 16:48:57 -0500

    Context factory fixed.

--- src/cicloso.bikes.core/Data/BikesContextFactory.cs
+++ src/cicloso.bikes.core/Data/BikesContextFactory.cs
@@ -9,10 +9,12 @@ public class BikesContextFactory : IDesignTimeDbContextFactory<BikesContext>
 {
     public BikesContext CreateDbContext(string[] args)
     {
-        var connectionString = Environment.GetEnvironmentVariable("BikesContext");
+        var connectionString =
+        args.FirstOrDefault(a => a.StartsWith("--connection="))?.Split('=')[1]
+        ?? Environment.GetEnvironmentVariable("BikesContext");
 
         if (string.IsNullOrEmpty(connectionString))
-            throw new Exception("Environment variable 'BikesContext' not found.");
+            throw new Exception("No connection string provided.");
 
         var optionsBuilder = new DbContextOptionsBuilder<BikesContext>();
         optionsBuilder.UseSqlServer(connectionString);
